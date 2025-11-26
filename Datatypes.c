13f2b33af9cbea6a82ad9f2f5aba9c27fd3d876d
Nikita Kalal <nikitakalal265@gmail.com>
2025-11-26 20:06:58 +0530

    Add files via upload
    C program of Student Information using Different data types

---
+++ Datatypes.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int studentID =15345;
+    float Studentfee =75.5;
+    char Bloodgroup ='B';
+    int Studentage = "21";
+    
+    
+    // Students Information
+
+    printf("Student ID is:%d\n",studentID);
+    printf(" Student Fees:%f\n",Studentfee);
+    printf("Students Blood Group is :%c\n",Bloodgroup);
+    printf("Students Age is:%d\n",Studentage);
+
+  
+
+}
