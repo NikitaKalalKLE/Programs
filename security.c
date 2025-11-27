c0eb9450012f1596b7f9060ac60ea84f2b9a8418
Nikita Kalal <nikitakalal265@gmail.com>
2025-11-27 07:17:23 +0530

    Add files via upload
    This program is about Security door lock

---
+++ security.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+// security codeprogram
+
+int main()
+{
+  int doorpwd=56563;
+
+  if("doorpwd==56563")
+  printf(" Correct code:", doorpwd);
+  else 
+  printf("Wrong code:", doorpwd);
+
+  return 0;
+
+
+}
